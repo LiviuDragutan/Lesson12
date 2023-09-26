1d04348bad3274d5dfd3c0eb35b669b5b0014d74
LiviuDragutan <liviu_dragutan@yahoo.com>
2023-09-26 16:02:28 +0300

    homework

---
+++ index.js
@@ -0,0 +1,44 @@
+const ziuaSaptaminii = 'Luni';
+
+switch (ziuaSaptaminii) {
+    case 'Luni':
+        console.log('Astazi este luni!')
+        case 'Marti':
+        console.log('Astazi este marti!')
+        case 'Miercuri':
+        console.log('Astazi este miercuri!')
+        case 'Joi':
+        console.log('Astazi este joi')
+        case 'Vineri':
+        console.log('Astazi este vineri!')
+        case 'Simbata':
+        console.log('Astazi este simbata!')
+        case 'Duminica':
+        console.log('Astazi este duminica!')
+}
+
+const str = 'str'
+const lungimeaStr = 'lungimeaStr'
+if (str>10)
+{ 
+    console.log('lungimea str este mai mare decit 10')
+}
+else{
+    console.log('lungimea str este mai mica decit 10')
+}
+
+const variabila1 = 'YES'
+console.log (variabila1.toLowerCase())
+
+const variabila2 = 'no'
+console.log (variabila2.toUpperCase())
+
+const variabila3 = 'rock music is the best'
+console.log(variabila3.slice(11, -9))
+
+// const variabila5 = 'rock music is the best'
+// const variabila6 = variabila5.slice(11, -9)
+// console.log(variabila6)
+
+const variabila7 = 'rock music is the best'
+console.log(variabila3.substring(11, 13))
